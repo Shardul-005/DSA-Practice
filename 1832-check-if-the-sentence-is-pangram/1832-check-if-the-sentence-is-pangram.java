3d95adb0b8e678adb0b91e81d8b7cbf0b2f2dcc8
Shardul-005 <2023bit005@sggs.ac.in>
2026-04-14 23:07:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,11 +1,15 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-       if(sentence.length()<26) return false;
-        for(char ch='a';ch<='z';ch++){
-            if(sentence.indexOf(ch)<0){
+        if(sentence.length() < 26){
+            return false;
+        }
+
+        for(char ch='a'; ch<='z'; ch++){
+            if(sentence.indexOf(ch) < 0){
                 return false;
             }
         }
+
         return true;
     } 
 }
